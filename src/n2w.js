2d449e071ccb2b89ac43d73ece8deb6b2e3fcf30
suganthsugi <suganthjayanthi@gmail.com>
2023-03-01 20:28:35 +0530

    updated convertNumberToTextIndian

--- src/n2w.js
+++ src/n2w.js
@@ -51,8 +51,8 @@
     }
     return text;
   }
-  
-  function convertNumberToTextIndian(num) {
+
+  function convertNumberToTextIndian(num){
     const ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
     const teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
