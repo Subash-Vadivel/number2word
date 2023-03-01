f8d6ead14a3f09d35ca855167d426107748b520c
suganthsugi <suganthjayanthi@gmail.com>
2023-03-01 20:27:37 +0530

    updated convertNumberToTextIndian

--- src/n2w.js
+++ src/n2w.js
@@ -53,59 +53,66 @@
   }
   
   function convertNumberToTextIndian(num) {
-    if (num > 9999999999 || num < -9999999999) {
-      throw new Error('Number out of range');
-    }
-    var tnum = num
-    if (tnum < 0) {
-      num = -num
-    }
-  
-    const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    const thousands = ['', 'thousand', 'lakh', 'crore'];
+    const ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    const teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
   
     if (num === 0) {
-      return 'zero';
+      return "zero";
     }
   
-    let text = '';
-    let i = 0;
+    if (num < -9999999999 || num > 9999999999) {
+      return "invalid number";
+    }
+    var tnum=num
+    if(num<0){
+      num=-num
+    }
+    let str = "";
   
-    while (num > 0) {
-      const hundreds = num % 1000;
-      if (hundreds > 0) {
-        let str = '';
-        if (hundreds < 10) {
-          str = ones[hundreds];
-        } else if (hundreds < 100) {
-          str = tens[Math.floor(hundreds / 10)];
-          if (hundreds % 10 > 0) {
-            str += ' ' + ones[hundreds % 10];
-          }
-        } else {
-          str = ones[Math.floor(hundreds / 100)] + ' hundred';
-          if (hundreds % 100 > 0) {
-            str += ' ' + convertNumberToText(hundreds % 100);
-          }
-        }
-        if (i > 0) {
-          str += ' ' + thousands[i];
-        }
-        if (text.length > 0) {
-          text = str + ' ' + text;
-        } else {
-          text = str;
+    if (Math.floor(num / 10000000) > 0) {
+      str += convertNumberToTextIndian(Math.floor(num / 10000000)) + " crore ";
+      num %= 10000000;
+    }
+  
+    if (Math.floor(num / 100000) > 0) {
+      str += convertNumberToTextIndian(Math.floor(num / 100000)) + " lakh ";
+      num %= 100000;
+    }
+  
+    if (Math.floor(num / 1000) > 0) {
+      str += convertNumberToTextIndian(Math.floor(num / 1000)) + " thousand ";
+      num %= 1000;
+    }
+  
+    if (Math.floor(num / 100) > 0) {
+      str += ones[Math.floor(num / 100)] + " hundred ";
+      num %= 100;
+    }
+  
+    if (num > 0) {
+      if (str !== "") {
+        str += "and ";
+      }
+  
+      if (num < 10) {
+        str += ones[num];
+      } else if (num < 20) {
+        str += teens[num - 10];
+      } else {
+        str += tens[Math.floor(num / 10)];
+        if (num % 10 > 0) {
+          str += " " + ones[num % 10];
         }
       }
-      num = Math.floor(num / 1000);
-      i++;
     }
-    if (tnum < 0) {
-      text = `minus ${text}`
+    if(tnum<0){
+      str='minus '+str
     }
-    return text;
+    return str.trim();
   }
+  
+  
 
   export const cn2t=(num)=>{
      return convertNumberToText(num);
