e0948db935ef1f2da6186414f1cab591c5f6f71c
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2023-03-02 09:45:35 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import './App.css';
 import { cn2t,cn2in } from './n2w';
 import { useState } from 'react';
@@ -10,30 +10,32 @@ export default function App() {
   const [no,setNo]=useState('');
   const [is,setIs]=useState();
   const[us,setUs]=useState();
-  const[to,setTo]=useState("");
-  const [lan,setLan]=useState("Tamil");
+  const[to,setTo]=useState('ta');
+  const [lan,setLan]=useState("");
+  const [res,setResult]=useState('');
+  const [lanout,setLanout]=useState("Select a language to Convert");
+
+  const clicked=(e)=>{
+    axios.get('https://translated-mymemory---translation-memory.p.rapidapi.com/get',{
+      params: {langpair: `en|${to}`, q: us },
+      headers: {
+        'X-RapidAPI-Key': '01dc308f17msha4ada22f271caa5p1d231fjsn8ba241b34e91',
+        'X-RapidAPI-Host': 'translated-mymemory---translation-memory.p.rapidapi.com'
+      },
+  }).then((res)=>{setResult(res.data.matches[0].translation);console.log(res.data.matches[0].translation);}).catch((err)=>{console.log(err)})
+  setLanout(lan)
+  e.preventDefault()
+  }
   const sub=(e)=>{
     setIs(cn2in(no));
     setUs(cn2t(no));
-    const data=new Object({
-      q:us,
-      target:to,
-      key: "text"
-    })
-    axios.post('https://translation.googleapis.com/language/translate/v2',data).then((res)=>{console.log(res)}).catch((err)=>{console.log(err)})
-
     e.preventDefault();
   }
-  return (
+
+
+   return (
     <Container fluid>
-      <Navbar expand="lg">
-        <Navbar.Brand href="#" className="mx-auto" style={{color:'white'}}>Number to Multilanguage Convertor</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="nav">
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <header>Converto</header>
       <Row className="align-items-center vh-100">
         <Col md={4} sm={3} xs={2}></Col>
         <Col md={4} sm={6} xs={8}>
@@ -44,7 +46,9 @@ export default function App() {
                   Number:
                 </label>
                 <input type="number" value={no} onChange={(e)=>{setNo(e.target.value)}} className="form-control" id="numberInput" />
+                <center>
                 <button onClick={sub} className='button-3'>Submit</button>
+                </center>
               </div>
               <div className="mb-3">
                 <label htmlFor="indianStandard" className="form-label">
@@ -62,16 +66,16 @@ export default function App() {
               <table className="table mb-3 mytable">
                 <thead>
                   <tr>
-                    <td>English</td>
-                    <td>Tamil</td>
-                    <td>Hindi</td>
+                    <td onClick={()=>{setLan("French");setTo("fr")}}>	French</td>
+              <td onClick={()=>{setLan("Tamil");setTo("ta")}}>Tamil</td>
+              <td onClick={()=>{setLan("Hindi");setTo("hi")}}>Hindi</td>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <td>Urdu</td>
-                    <td>Telugu</td>
-                    <td>Malayalam</td>
+                  <td onClick={()=>{setLan("de");setTo("German")}}>German</td>
+              <td onClick={()=>{setLan("Russian");setTo("ru")}}>Russian</td>
+              <td onClick={()=>{setLan("Spanish");setTo("es")}}>Spanish</td>
                   </tr>
                 </tbody>
               </table>
@@ -82,16 +86,20 @@ export default function App() {
                   className="form-control"
                   id="customConvertInput"
                   placeholder="Custom Convert"
+                  value={lan}
+                  onChange={(e)=>{setLan(e.target.value)}}
                 />
               </div>
-              <button type="submit" className="button-3">
+              <center>
+              <button onClick={clicked} className="button-3">
                 Convert
               </button>
+              </center>
               <div className="mb-3">
                <br/> <label htmlFor="tamilOutput" className="form-label">
-                  {lan}
+                  {lanout}
                 </label>
-                <textarea className="form-control" id="tamilOutput"></textarea>
+                <textarea className="form-control" id="tamilOutput" value={res}></textarea>
               </div>
             </form>
           </div>
