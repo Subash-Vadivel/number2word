e0b0b91355c87aa33f1c7b1d9d799fa831aef9cb
R <106548433+ragulkarthick4@users.noreply.github.com>
2023-03-02 09:06:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,77 +1,104 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
 import { cn2t,cn2in } from './n2w';
-import { Container, Row, Col } from 'react-bootstrap';
-import { useState} from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
-function App() {
+export default function App() {
   const [no,setNo]=useState('');
   const [is,setIs]=useState();
   const[us,setUs]=useState();
-  const[to,setTo]=useState('ta');
-  const [lan,setLan]=useState("");
-  const [res,setResult]=useState('');
-  const [lanout,setLanout]=useState("Select a language to Convert");
-
-  const clicked=(e)=>{
-    axios.get('https://translated-mymemory---translation-memory.p.rapidapi.com/get',{
-      params: {langpair: `en|${to}`, q: us },
-      headers: {
-        'X-RapidAPI-Key': '01dc308f17msha4ada22f271caa5p1d231fjsn8ba241b34e91',
-        'X-RapidAPI-Host': 'translated-mymemory---translation-memory.p.rapidapi.com'
-      },
-  }).then((res)=>{setResult(res.data.matches[0].translation);console.log(res.data.matches[0].translation);}).catch((err)=>{console.log(err)})
-  setLanout(lan)
-  e.preventDefault()
-  }
+  const[to,setTo]=useState("");
+  const [lan,setLan]=useState("Tamil");
   const sub=(e)=>{
     setIs(cn2in(no));
     setUs(cn2t(no));
+    const data=new Object({
+      q:us,
+      target:to,
+      key: "text"
+    })
+    axios.post('https://translation.googleapis.com/language/translate/v2',data).then((res)=>{console.log(res)}).catch((err)=>{console.log(err)})
+
     e.preventDefault();
   }
   return (
-    <>
-    <Container>
-      <Row className="align-items-center viewport-height">
-        <Col md={4} sm={3} xs={2}>
+    <Container fluid>
+      <Navbar expand="lg">
+        <Navbar.Brand href="#" className="mx-auto" style={{color:'white'}}>Number to Multilanguage Convertor</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="nav">
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Row className="align-items-center vh-100">
+        <Col md={4} sm={3} xs={2}></Col>
+        <Col md={4} sm={6} xs={8}>
+          <div className="d-flex flex-column align-items-center">
+            <form className="w-100">
+              <div className="mb-3">
+                <label htmlFor="numberInput" className="form-label">
+                  Number:
+                </label>
+                <input type="number" value={no} onChange={(e)=>{setNo(e.target.value)}} className="form-control" id="numberInput" />
+                <button onClick={sub} className='button-3'>Submit</button>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="indianStandard" className="form-label">
+                  Indian Standard:
+                </label>
+                <textarea value={is} className="form-control" id="indianStandard"></textarea>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="usStandard" className="form-label">
+                  US Standard:
+                </label>
+                <textarea value={us} className="form-control" id="usStandard"></textarea>
+              </div>
+              <div className='table-responsive'>
+              <table className="table mb-3 mytable">
+                <thead>
+                  <tr>
+                    <td>English</td>
+                    <td>Tamil</td>
+                    <td>Hindi</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Urdu</td>
+                    <td>Telugu</td>
+                    <td>Malayalam</td>
+                  </tr>
+                </tbody>
+              </table>
+              </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="customConvertInput"
+                  placeholder="Custom Convert"
+                />
+              </div>
+              <button type="submit" className="button-3">
+                Convert
+              </button>
+              <div className="mb-3">
+               <br/> <label htmlFor="tamilOutput" className="form-label">
+                  {lan}
+                </label>
+                <textarea className="form-control" id="tamilOutput"></textarea>
+              </div>
+            </form>
+          </div>
         </Col>
-        <Col md={4} sm={3} xs={2}>
-        <center>
-          <Container className='container-wrapper'>
-
-         <form>
-          <span className='float-start'><b>Number:</b></span>
-          &nbsp;<input type="text" value={no} onChange={(e)=>{setNo(e.target.value)}}></input>&nbsp;&nbsp;<button className='button-3' onClick={sub}>Submit</button><br/>
-          <span className='float-start special'><b>Indian Standard:</b></span><textarea value={is}></textarea><br/>
-          <span className='float-start special'><b>US Standard:</b></span> <textarea value={us}></textarea><br></br><br></br>
-          <table className='mytable' cellPadding={10}>
-            <tr>
-              <td onClick={()=>{setLan("French");setTo("fr")}}>	French</td>
-              <td onClick={()=>{setLan("Tamil");setTo("ta")}}>Tamil</td>
-              <td onClick={()=>{setLan("Hindi");setTo("hi")}}>Hindi</td>
-            </tr>
-            <tr>
-              <td onClick={()=>{setLan("de");setTo("German")}}>German</td>
-              <td onClick={()=>{setLan("Russian");setTo("ru")}}>Russian</td>
-              <td onClick={()=>{setLan("Spanish");setTo("es")}}>Spanish</td>
-            </tr>
-          </table><br/>
-          <input type="text" placeholder='Select Language' value={lan} onChange={(e)=>{setLan(e.target.val)}}></input>&nbsp;&nbsp;<button className='button-3' onClick={clicked}>Convert</button>
-          <br></br><br></br>
-          <span className='float-start special'><b>{lanout}:</b></span>
-          <textarea value={res}></textarea>
-         </form>
-
-         </Container>
-        </center>
-        </Col>
-        <Col md={4} sm={3} xs={2}>
-        </Col>
+        <Col md={4} sm={3} xs={2}></Col>
       </Row>
     </Container>
-    </>
   );
 }
 
-export default App;
